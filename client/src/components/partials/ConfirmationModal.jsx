ec759e4bf34cb5abb7bdd2c4503fd0b8f9996b57
axl6409 <alexandre.celier64@gmail.com>
2023-09-28 22:25:07 +0200

    Create Leagues & Teams

--- client/src/components/partials/ConfirmationModal.jsx
+++ client/src/components/partials/ConfirmationModal.jsx
@@ -1,10 +1,24 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCaretLeft, faCheck, faXmark} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
 const ConfirmationModal = ({ message, onConfirm, onCancel }) => {
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <p>{message}</p>
-        <button onClick={onConfirm}>Confirmer</button>
-        <button onClick={onCancel}>Annuler</button>
+    <div className="modal relative w-full py-1.5">
+      <div className="modal-content py-4 px-6 mx-auto border-2 border-black w-[95%] my-auto block">
+        <p className="font-sans text-center font-l leading-7">{message}</p>
+        <div className="flex flex-row justify-evenly">
+          <button
+            className="w-fit block relative my-4 before:content-[''] before:inline-block before:absolute before:z-[-1] before:inset-0 before:bg-green-lime before:border-black before:border-2 group"
+            onClick={onConfirm}>
+            <span className="relative z-[2] w-full block border-2 border-black text-black px-3 py-1 text-center shadow-md bg-white transition -translate-y-1 translate-x-1 group-hover:-translate-y-0 group-hover:-translate-x-0"><FontAwesomeIcon icon={faCheck} /></span>
+          </button>
+          <button
+            className="w-fit block relative my-4 before:content-[''] before:inline-block before:absolute before:z-[-1] before:inset-0 before:bg-flat-red before:border-black before:border-2 group"
+            onClick={onCancel}>
+            <span className="relative z-[2] w-full block border-2 border-black text-black px-3 py-1 text-center shadow-md bg-white transition -translate-y-1 translate-x-1 group-hover:-translate-y-0 group-hover:-translate-x-0"><FontAwesomeIcon icon={faXmark} /></span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -185,6 +185,19 @@ router.put('/admin/teams/edit/:id', async (req, res) => {
   }
 });
 
-router.delete('/admin/teams/delete/:id', authenticateJWT, TeamController.deleteTeam);
+router.delete('/admin/teams/delete/:id', authenticateJWT, async (req, res) => {
+  try {
+    if (req.user.role !== 'admin') return res.status(403).json({ error: 'Accès non autorisé', message: req.user });
+
+    const teamId = req.params.id;
+    const team = await Team.findByPk(teamId);
+    if (!team) return res.status(404).json({ error: 'Équipe non trouvée' });
+
+    await team.destroy();
+    res.status(200).json({ message: 'Équipe supprimée avec succès' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la suppression de l’équipe', message: error.message });
+  }
+});
 
 module.exports = router;
