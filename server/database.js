b1686dfbe307d2b87f1e96b119c4e68be85b28aa
Alexandre Celier <50583984+axl6409@users.noreply.github.com>
2023-11-17 12:01:59 +0100

    config for new postgre db

--- server/database.js
+++ server/database.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: console.log
