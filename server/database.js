014ddfb2df0555eadd33ea96cf440faf9366a423
Alexandre Celier <50583984+axl6409@users.noreply.github.com>
2023-11-17 12:22:26 +0100

    Modif corsOptions

--- server/database.js
+++ server/database.js
@@ -4,6 +4,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: console.log
 });
 
