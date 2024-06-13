5f0cf6eeca016d9b2d5265db3409e98704931fc6
axl6409 <alexandre.celier64@gmail.com>
2024-06-13 11:44:43 +0200

    Modif for Heroku database PostgreSQL

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
 
 require('dotenv').config();
-export const authAxios = axios.create({
-  baseURL: process.env.SERVER_BASE_URL,
-  timeout: 5000
-})
 
-export const publicAxios = axios.create({
-  baseURL: process.env.PUBLIC_BASE_URL,
-  timeout: 5000
-})
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://127.0.0.1:3001/api',
+  withCredentials: true
+});
+
+export default api;
