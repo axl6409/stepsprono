fafdc9ff3422297dcd86fc27e67dd3c9fb0946c7
axl6409 <alexandre.celier64@gmail.com>
2024-06-13 12:03:25 +0200

    Modif for Heroku database PostgreSQL

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-require('dotenv').config();
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_APP_API_URL : 'http://127.0.0.1:3001/api',
   withCredentials: true
