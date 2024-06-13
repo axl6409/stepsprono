e2d389493d8e837f1f45bbc26af3361f654aefc1
axl6409 <alexandre.celier64@gmail.com>
2024-06-13 11:33:17 +0200

    Modif for Heroku database PostgreSQL

--- server/database.js
+++ server/database.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: console.log
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
