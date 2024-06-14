a854d76ca77f64547aa25f6717ea91f1c2259264
axl6409 <alexandre.celier64@gmail.com>
2024-06-14 12:18:08 +0200

    Values for presentation

--- server/src/services/teamService.js
+++ server/src/services/teamService.js
@@ -185,6 +185,7 @@ async function updateTeamsRanking(teamId = null, competitionId = null) {
       }
     };
     const response = await axios.request(options);
+    console.log(response.data.response)
     const teams = response.data.response[0].league.standings[0];
 
     if (teamId) {
