92012931a916d427fe5c231a98d4fd8eda1f8d73
axl6409 <alexandre.celier64@gmail.com>
2024-06-14 11:48:08 +0200

    Values for presentation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
