3ae73f945a8128430b85f8b6860f2deb63f40487
axl6409 <alexandre.celier64@gmail.com>
2024-07-10 16:20:28 +0200

    Refactor database connexion

--- server/database.js
+++ server/database.js
@@ -1,38 +1,4 @@
-const { Sequelize } = require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
-const isProduction = env === 'production';
-
-let sequelize;
-
-if (isProduction) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    define: {
-      underscored: true,
-      freezeTableName: true,
-    }
-  });
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: false
-    },
-    define: {
-      underscored: false,
-      freezeTableName: false,
-    }
-  });
-}
+const getSequelizeInstance = require('./dbConfig');
+const sequelize = getSequelizeInstance();
 
 module.exports = sequelize;
---
+++ server/dbConfig.js
@@ -0,0 +1,40 @@
+const { Sequelize } = require('sequelize');
+
+function getSequelizeInstance() {
+  const env = process.env.NODE_ENV || 'development';
+  const isProduction = env === 'production';
+
+  let sequelize;
+  if (isProduction) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      define: {
+        underscored: true,
+        freezeTableName: true,
+      }
+    });
+  } else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: false
+      },
+      define: {
+        underscored: false,
+        freezeTableName: false,
+      }
+    });
+  }
+  return sequelize;
+}
+
+module.exports = getSequelizeInstance;
