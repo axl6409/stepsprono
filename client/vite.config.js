408867dd2777875a011dfb530772ede6e35457f4
axl6409 <alexandre.celier64@gmail.com>
2024-08-14 15:34:11 +0200

    Vite config for env vars at root

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const root = path.resolve(__dirname, '../');
+  const env = loadEnv(mode, root, '');
 
   return {
     plugins: [react()],
@@ -12,7 +14,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      'import.meta.env': env,
+      'import.meta.env': env
     },
     server: {
       proxy: {
